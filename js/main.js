8bdf6eb5c0aa7b6fb65fbc4f0b261623b0b9995c
T-Damer <dpancov64@gmail.com>
2021-08-05 14:02:02 +0300

    Fixed clock logic

--- js/main.js
+++ js/main.js
@@ -1,21 +1,19 @@
-// CLOCK ROTATION AND DATE 
-var hours = document.getElementById('clock-hours'),
-minutes = document.getElementById('clock-minutes'),
-seconds = document.getElementById('clock-seconds'),
+// CLOCK ROTATION AND DATE
+const hours = document.getElementById('clock-hours')
+const minutes = document.getElementById('clock-minutes')
+const seconds = document.getElementById('clock-seconds')
 
-var clock = () => {
-    var date = new Date()
+const clock = () => {
+  let date = new Date()
 
-    var hh = date.getHours() * 30,
-    mm = date.getMinutes() * 6,
-    ss = date.getSeconds() * 6
+  let hh = date.getHours() * 30
+  let mm = date.getMinutes() * 6
+  let ss = date.getSeconds() * 6
 
-    hours.style.transform = `rotateZ(${hh + mm / 12}deg)`
-    minutes.style.transform = `rotateZ(${mm}deg)`
-    seconds.style.transform = `rotateZ(${ss}deg)`
+  hours.style.transform = `rotateZ(${hh + mm / 12}deg)`
+  minutes.style.transform = `rotateZ(${mm}deg)`
+  seconds.style.transform = `rotateZ(${ss}deg)`
 }
 
 setInterval(clock, 1000) // 1000 = 1s
-// CLOCK TEXT 
-
-
+// CLOCK TEXT
