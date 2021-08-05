3bbd1f9196cc84d24bdf12abfb86fd14978e8a16
T-Damer <dpancov64@gmail.com>
2021-08-05 15:36:23 +0300

    Added hh-mm under clock

--- js/main.js
+++ js/main.js
@@ -13,17 +13,59 @@ const clock = () => {
   hours.style.transform = `rotateZ(${hh + mm / 12}deg)`
   minutes.style.transform = `rotateZ(${mm}deg)`
   seconds.style.transform = `rotateZ(${ss}deg)`
-}
 
+  return { date }
+}
 setInterval(clock, 1000) // 1000 = 1s
 
 // CLOCK:TEXT
-
 const textHours = document.getElementById('text-hours'),
   textMinutes = document.getElementById('text-minutes'),
-  textAmPm = document.getElementById('text-AmPm'),
+  textAmPm = document.getElementById('text-ampm'),
   dateDay = document.getElementById('date-day'),
   dateMonth = document.getElementById('date-month'),
   dateYear = document.getElementById('date-year')
 
-const clockText = () => {}
+const clockText = () => {
+  // Reusing date to be more accurate
+  let { date } = clock()
+  let hh = date.getHours(),
+    mm = date.getMinutes(),
+    ss = date.getSeconds(),
+    ampm,
+    month = date.getMonth(),
+    year = date.getFullYear()
+
+  // Establishing am-pm
+  if (hh >= 12) {
+    hh = hh - 12
+    ampm = 'PM'
+  } else {
+    ampm = 'AM'
+  }
+
+  // Show am or pm
+  textAmPm.innerHTML = ampm
+
+  // Show zero before hh
+  if (hh < 10) {
+    hh = `0${hh}`
+  }
+
+  // Show hours
+  textHours.innerHTML = `${hh}:`
+
+  // Detecting 0 AM and changing to 12 AM
+  if (hh == 0) {
+    hh = 12
+  }
+
+  // Show mm
+  textMinutes.innerHTML = mm
+
+  // Show zero before hh
+  if (mm < 10) {
+    mm = `0${mm}`
+  }
+}
+setInterval(clockText, 1000) // 1000 = 1s
